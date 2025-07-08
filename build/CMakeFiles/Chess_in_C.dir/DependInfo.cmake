438530d3111d8158ef4427beeb4252797dbad600
Judgementoes <102240051@student.vgu.edu.vn>
2025-07-08 18:06:55 +0700

    changed compiler to MinGW

---
+++ build/CMakeFiles/Chess_in_C.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/C_Projects_Folder/First_year_Final_Project/src/ChessBoard.c" "CMakeFiles/Chess_in_C.dir/src/ChessBoard.c.obj" "gcc" "CMakeFiles/Chess_in_C.dir/src/ChessBoard.c.obj.d"
+  "E:/C_Projects_Folder/First_year_Final_Project/src/chess_FEN.c" "CMakeFiles/Chess_in_C.dir/src/chess_FEN.c.obj" "gcc" "CMakeFiles/Chess_in_C.dir/src/chess_FEN.c.obj.d"
+  "E:/C_Projects_Folder/First_year_Final_Project/src/chess_board.c" "CMakeFiles/Chess_in_C.dir/src/chess_board.c.obj" "gcc" "CMakeFiles/Chess_in_C.dir/src/chess_board.c.obj.d"
+  "E:/C_Projects_Folder/First_year_Final_Project/src/chess_checksandpins.c" "CMakeFiles/Chess_in_C.dir/src/chess_checksandpins.c.obj" "gcc" "CMakeFiles/Chess_in_C.dir/src/chess_checksandpins.c.obj.d"
+  "E:/C_Projects_Folder/First_year_Final_Project/src/chess_game.c" "CMakeFiles/Chess_in_C.dir/src/chess_game.c.obj" "gcc" "CMakeFiles/Chess_in_C.dir/src/chess_game.c.obj.d"
+  "E:/C_Projects_Folder/First_year_Final_Project/src/chess_moves.c" "CMakeFiles/Chess_in_C.dir/src/chess_moves.c.obj" "gcc" "CMakeFiles/Chess_in_C.dir/src/chess_moves.c.obj.d"
+  "E:/C_Projects_Folder/First_year_Final_Project/src/chess_piece_data.c" "CMakeFiles/Chess_in_C.dir/src/chess_piece_data.c.obj" "gcc" "CMakeFiles/Chess_in_C.dir/src/chess_piece_data.c.obj.d"
+  "E:/C_Projects_Folder/First_year_Final_Project/src/chess_unmake.c" "CMakeFiles/Chess_in_C.dir/src/chess_unmake.c.obj" "gcc" "CMakeFiles/Chess_in_C.dir/src/chess_unmake.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Chess_in_C.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess_in_C.dir/src/ChessBoard.c.obj"
+  "CMakeFiles/Chess_in_C.dir/src/ChessBoard.c.obj.d"
+  "CMakeFiles/Chess_in_C.dir/src/chess_FEN.c.obj"
+  "CMakeFiles/Chess_in_C.dir/src/chess_FEN.c.obj.d"
+  "CMakeFiles/Chess_in_C.dir/src/chess_board.c.obj"
+  "CMakeFiles/Chess_in_C.dir/src/chess_board.c.obj.d"
+  "CMakeFiles/Chess_in_C.dir/src/chess_checksandpins.c.obj"
+  "CMakeFiles/Chess_in_C.dir/src/chess_checksandpins.c.obj.d"
+  "CMakeFiles/Chess_in_C.dir/src/chess_game.c.obj"
+  "CMakeFiles/Chess_in_C.dir/src/chess_game.c.obj.d"
+  "CMakeFiles/Chess_in_C.dir/src/chess_moves.c.obj"
+  "CMakeFiles/Chess_in_C.dir/src/chess_moves.c.obj.d"
+  "CMakeFiles/Chess_in_C.dir/src/chess_piece_data.c.obj"
+  "CMakeFiles/Chess_in_C.dir/src/chess_piece_data.c.obj.d"
+  "CMakeFiles/Chess_in_C.dir/src/chess_unmake.c.obj"
+  "CMakeFiles/Chess_in_C.dir/src/chess_unmake.c.obj.d"
+  "E:/C_Projects_Folder/First_year_Final_Project/Chess_in_C.exe"
+  "E:/C_Projects_Folder/First_year_Final_Project/Chess_in_C.exe.manifest"
+  "E:/C_Projects_Folder/First_year_Final_Project/Chess_in_C.pdb"
+  "libChess_in_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chess_in_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
